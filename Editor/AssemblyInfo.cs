53faee8cecb31dd9588d0eea1c2e6d395b1df41d
Daniel Schulz <faulolio@gmail.com>
2025-08-28 17:54:30 +0200

    build: InternalsVisibleTo

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -2,3 +2,5 @@ using System.Runtime.CompilerServices;
 using Slothsoft.UnityExtensions;
 
 [assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_TESTS_EDITMODE)]
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_TESTS_PLAYMODE)]
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_PROXYGEN)]
